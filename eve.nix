2266b50919987c121f7dff43128508b037e3ed38
Tripleo <tripleo@tripleo.milkyway>
2025-04-16 07:33:55 -0400

    Use lazy copy

--- eve.nix
+++ eve.nix
@@ -1,10 +1,7 @@
 {pkgs}: let
-  eveSrc = pkgs.fetchFromGitHub {
-    owner = "witheve";
-    repo = "Eve";
-    rev = "f680d331d827c7dfe3eaeb79a2a7ce86710d930b";
-    sha256 = "sha256-HOX3ojJ5VvHaf8eI9F4u9dDhV+yislEmy7ei0//U3hA=";
-  };
+  # fixme we want this, or something like it (do it later)
+  #    sha256 = "sha256-HOX3ojJ5VvHaf8eI9F4u9dDhV+yislEmy7ei0//U3hA=";
+  eveSrc = ./.;
 
   yarnModules = pkgs.mkYarnModules {
     pname = "eve";
@@ -16,7 +13,7 @@
 in
   pkgs.stdenv.mkDerivation {
     pname = "eve";
-    version = "0.3-alpha";
+    version = "0.3-alpha.git-f680d331d827c7dfe3eaeb79a2a7ce86710d930b";
     src = eveSrc;
     buildInputs = [pkgs.nodejs pkgs.yarn];
     nativeBuildInputs = [yarnModules];
